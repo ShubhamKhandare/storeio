a5ef45e9ebdc339df594077a93aad7a1484061c1
shubhamk <shubham.khandare@gslab.com>
2021-03-23 11:49:29 +0530

    PG database support.

--- storeio/settings.py
+++ storeio/settings.py
@@ -78,10 +78,18 @@ WSGI_APPLICATION = 'storeio.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'storeio',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    },
 }
 
 # Password validation
